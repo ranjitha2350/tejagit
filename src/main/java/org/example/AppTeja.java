dfde7062711462f06d8e713e887a4e611b7ff06b
rvatti920 <ranjitha_vattipulusu@comcast.com>
2024-02-12 23:11:11 +0530

    new prints

--- src/main/java/org/example/AppTeja.java
+++ src/main/java/org/example/AppTeja.java
@@ -9,5 +9,7 @@ public class AppTeja
     public static void main( String[] args )
     {
         System.out.println( "Hello World! hi" );
+        System.out.println( "mrng" );
+        System.out.println("hi");
     }
 }
